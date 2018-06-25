c86fb635a5dc7e128909fa7f5f160f3a5695607c
Ivan-Sapronov <sapronov_ivan@mail.ru>
2018-06-25 08:44:40 +0300

    Отредактировал дату и аннотации

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 /**
- *Geekbrains.ru - Algoritms in Java, Lesson 1
+ *Geekbrains.ru - Learn Git, Lesson 1
  *@avtor Ivan Sapronov
- *@version dated May 15, 2018
+ *@version dated June 25, 2018
  *@GitHub: https://github.com/Ivan-Sapronov
  */
 
 public class Main {
     public static void main(String[] args) {
-		//Учусь работать с github.com
+		//“чусь работать с github.com
     }
 }
 
