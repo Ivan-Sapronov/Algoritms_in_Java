733f3090d3fd213ef4fe2cd410a59876cb6281ed
Ivan-Sapronov <sapronov_ivan@mail.ru>
2018-06-25 08:41:58 +0300

    Первый урок рабоы с GitHub

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ *Geekbrains.ru - Algoritms in Java, Lesson 1
+ *@avtor Ivan Sapronov
+ *@version dated May 15, 2018
+ *@GitHub: https://github.com/Ivan-Sapronov
+ */
+
+public class Main {
+    public static void main(String[] args) {
+		//Учусь работать с github.com
+    }
+}
+
+
